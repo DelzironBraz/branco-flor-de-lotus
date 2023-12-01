2693bbe30818b5ba48322f0864068df244fcb66e
Tiago Antunes Balieiro <tiagoantunes0905@gmail.com>
2023-12-01 06:19:31 -0300

    Criação da sidebar

---
+++ frontend/src/pages/Grafico.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Grafico = () => {
+    return (
+        <div>
+            <h1>Página do gráfico de progresso
+            </h1>
+        </div>
+    );
+};
+
+export default Grafico;
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Aqui é a Homepage</h1>
+        </div>
+    );
+};
+
+export default Homepage;
